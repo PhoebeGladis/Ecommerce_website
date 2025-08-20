5552152a4a79a03eb8c2f0367f90a705c7e91e7a
PhoebeGladis <phoebekaitha11@gmail.com>
2025-08-20 19:29:38 +0530

    Comitted changes

---
+++ routes/authRoute.js
@@ -0,0 +1,42 @@
+import express from "express";
+import {
+  registerController,
+  loginController,
+  testController,
+  forgotPasswordController,
+  updateProfileController,
+  getOrderController,
+  getAllOrderController,
+  orderStatusController,
+  getAllusersController,
+} from "../controllers/authController.js";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+const router = express.Router();
+
+router.post("/register", registerController);
+
+router.post("/login", loginController);
+
+router.post("/forgot-password", forgotPasswordController);
+
+router.get("/test", requireSignIn, isAdmin, testController);
+
+router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+
+router.put("/profile", requireSignIn, updateProfileController);
+
+router.get("/orders", requireSignIn, getOrderController);
+
+router.get("/all-orders", requireSignIn, getAllOrderController);
+
+router.put("/order-status/:orderId",requireSignIn,isAdmin,orderStatusController)
+
+router.get("/users",requireSignIn,isAdmin,getAllusersController)
+
+export default router;
